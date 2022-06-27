8c55466e97e86288d7c00a6bd978d3c362fc8db3
Justin Kulikauskas <jkulikau@redhat.com>
2022-06-27 14:06:55 -0400

    Implement initial core spec and status
    The fields added are present in all existing policy controllers.
    
    The NamespaceSelector is implemented differently than previously, but
    captures the previous behavior and extends it. The fuzz tests were
    partly just for practice.
    
    IsEnforce and IsInform are methods as opposed to (effectively) using an
    enum, because that would basically require/enforce a consistent
    capitalization, which could be a change in behavior. This has the
    potential to be extended in the future.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

---
+++ api/v1beta1/namespaceselector_test.go
@@ -0,0 +1,185 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package v1beta1
+
+import (
+	"errors"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"k8s.io/apimachinery/pkg/util/validation"
+)
+
+var sampleNamespaces = []string{
+	"foo", "bar", "baz", "boo", "default", "kube-one", "kube-two", "kube-three",
+}
+
+// Fuzz test to verify that excluding "*" always matches 0 namespaces. The
+// `Include` list and the input namespaces are both fuzzed.
+func FuzzMatchesExcludeAll(f *testing.F) {
+	for _, tc := range []string{"", "*", "?", "foo", "quux"} {
+		f.Add(tc, tc, tc)
+	}
+
+	f.Fuzz(func(t *testing.T, inc1, inc2, extraNS string) {
+		errs := validation.IsDNS1123Label(extraNS)
+		if len(errs) != 0 { // K8s Namespaces must be valid RFC 1123 DNS labels.
+			t.Skip()
+		}
+
+		inc := []NonEmptyString{NonEmptyString(inc1), NonEmptyString(inc2)}
+		sel := NamespaceSelector{Include: inc, Exclude: []NonEmptyString{"*"}}
+
+		got, err := sel.matches(append(sampleNamespaces, extraNS))
+		if err != nil {
+			if errors.Is(err, filepath.ErrBadPattern) {
+				t.Skip()
+			}
+			t.Errorf("Unexpected error '%v' when including '%v' and '%v', with ns '%v'",
+				err, inc1, inc2, extraNS)
+		}
+		if len(got) != 0 {
+			t.Errorf("Got non-empty matches '%v', when including '%v' and '%v', with ns '%v'",
+				got, inc1, inc2, extraNS)
+		}
+	})
+}
+
+// Fuzz test to verify that including "*" will always match all namespaces when
+// excludes is empty. Additional items in the `Include` list and the input
+// namespaces are both fuzzed.
+func FuzzMatchesIncludeAll(f *testing.F) {
+	for _, tc := range []string{"", "*", "?", "foo", "quux"} {
+		f.Add(tc, tc, tc)
+	}
+
+	f.Fuzz(func(t *testing.T, inc1, inc2, extraNS string) {
+		errs := validation.IsDNS1123Label(extraNS)
+		if len(errs) != 0 { // K8s Namespaces must be valid RFC 1123 DNS labels.
+			t.Skip()
+		}
+
+		inc := []NonEmptyString{"*", NonEmptyString(inc1), NonEmptyString(inc2)}
+		sel := NamespaceSelector{Include: inc, Exclude: []NonEmptyString{}}
+
+		got, err := sel.matches(append(sampleNamespaces, extraNS))
+		if err != nil {
+			if errors.Is(err, filepath.ErrBadPattern) {
+				t.Skip()
+			}
+			t.Errorf("Unexpected error '%v' when including '%v' and '%v', with ns '%v'",
+				err, inc1, inc2, extraNS)
+		}
+
+		desiredLen := len(sampleNamespaces) + 1
+		for _, ns := range sampleNamespaces {
+			if ns == extraNS {
+				desiredLen--
+			}
+		}
+
+		if len(got) != desiredLen {
+			t.Errorf("Incorrect matches '%v', when including '%v' and '%v', with ns '%v'",
+				got, inc1, inc2, extraNS)
+		}
+	})
+}
+
+func TestMatches(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		inc  []NonEmptyString
+		exc  []NonEmptyString
+		want []string
+	}{
+		"include trailing wildcard": {
+			inc:  []NonEmptyString{"kube-*"},
+			exc:  []NonEmptyString{},
+			want: []string{"kube-one", "kube-two", "kube-three"},
+		},
+		"exlude trailing wildcard": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"kube-*"},
+			want: []string{"foo", "bar", "baz", "boo", "default"},
+		},
+		"include leading wildcard": {
+			inc:  []NonEmptyString{"*o"},
+			exc:  []NonEmptyString{},
+			want: []string{"foo", "boo", "kube-two"},
+		},
+		"exlude leading wildcard": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"*o"},
+			want: []string{"bar", "baz", "default", "kube-one", "kube-three"},
+		},
+		"include middle wildcard": {
+			inc:  []NonEmptyString{"*a*"},
+			exc:  []NonEmptyString{},
+			want: []string{"bar", "baz", "default"},
+		},
+		"exclude middle wildcard": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"*a*"},
+			want: []string{"foo", "boo", "kube-one", "kube-two", "kube-three"},
+		},
+		"include one specific": {
+			inc:  []NonEmptyString{"foo"},
+			exc:  []NonEmptyString{},
+			want: []string{"foo"},
+		},
+		"exclude one specific": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"foo"},
+			want: []string{"bar", "baz", "boo", "default", "kube-one", "kube-two", "kube-three"},
+		},
+		"include multiple specific": {
+			inc:  []NonEmptyString{"foo", "bar", "kube-three"},
+			exc:  []NonEmptyString{},
+			want: []string{"foo", "bar", "kube-three"},
+		},
+		"exclude multiple specific": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"foo", "bar", "kube-three"},
+			want: []string{"baz", "boo", "default", "kube-one", "kube-two"},
+		},
+		"include single char wildcards": {
+			inc:  []NonEmptyString{"?a?"},
+			exc:  []NonEmptyString{},
+			want: []string{"bar", "baz"},
+		},
+		"exclude single char wildcards": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"?a?"},
+			want: []string{"foo", "boo", "default", "kube-one", "kube-two", "kube-three"},
+		},
+		"include character class": {
+			inc:  []NonEmptyString{"[fb]oo"},
+			exc:  []NonEmptyString{},
+			want: []string{"foo", "boo"},
+		},
+		"exclude character class": {
+			inc:  []NonEmptyString{"*"},
+			exc:  []NonEmptyString{"[fb]oo"},
+			want: []string{"bar", "baz", "default", "kube-one", "kube-two", "kube-three"},
+		},
+	}
+
+	for name, tcase := range tests {
+		sel := NamespaceSelector{Include: tcase.inc, Exclude: tcase.exc}
+
+		got, err := sel.matches(sampleNamespaces)
+		if err != nil {
+			t.Errorf("Unexpected error '%v', in test '%v'", err, name)
+		}
+
+		less := func(a, b string) bool { return a < b }
+		diff := cmp.Diff(tcase.want, got, cmpopts.SortSlices(less))
+
+		if diff != "" {
+			t.Errorf("Mismatch in test '%v': %v", name, diff)
+		}
+	}
+}
--- api/v1beta1/policycore_types.go
+++ api/v1beta1/policycore_types.go
@@ -3,27 +3,96 @@
 // Package v1beta1 contains API Schema definitions for the policy v1beta1 API group
 //+kubebuilder:object:generate=true
 //+groupName=policy.open-cluster-management.io
+//+kubebuilder:validation:Optional
 package v1beta1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// NOTE: json tags are required. Any new fields you add must have json tags for
+// the fields to be serialized.
 // Important: Run "make" to regenerate code after modifying this file
 
-// PolicyCoreSpec defines the desired state of PolicyCore
+// PolicyCoreSpec defines fields that policies must implement to be part of the
+// Open Cluster Management policy framework. The intention is for controllers
+// to embed this struct in their *Spec definitions.
 type PolicyCoreSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Severity defines how serious the situation is when the policy is not
+	// compliant. The severity should not change the behavior of the policy, but
+	// may be read and used by other tools. Accepted values include: low,
+	// medium, high, and critical.
+	Severity Severity `json:"severity,omitempty"`
 
-	// Foo is an example field of PolicyCore. Edit policycore_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// RemediationAction indicates what the policy controller should do when the
+	// policy is not compliant. Accepted values include inform, and enforce.
+	// Note that not all policy controllers will attempt to automatically
+	// remediate a policy, even when set to "enforce".
+	RemediationAction RemediationAction `json:"remediationAction,omitempty"`
+
+	// NamepaceSelector indicates which namespaces on the cluster this policy
+	// should apply to, when the policy applies to namespaced objects.
+	NamespaceSelector NamespaceSelector `json:"namespaceSelector,omitempty"`
 }
 
-// PolicyCoreStatus defines the observed state of PolicyCore
+//+kubebuilder:validation:Enum=low;Low;medium;Medium;high;High;critical;Critical
+type Severity string
+
+//+kubebuilder:validation:Enum=Inform;inform;Enforce;enforce
+type RemediationAction string
+
+// IsEnforce is true when the policy controller can attempt to enforce the
+// policy by remediating it automatically. Note that not all controllers will
+// support automatic enforcement.
+func (ra RemediationAction) IsEnforce() bool {
+	return ra == "Enforce" || ra == "enforce"
+}
+
+// IsInform is true when the policy controller should only report whether the
+// policy is compliant or not and should not perform any actions to attempt
+// remediation.
+func (ra RemediationAction) IsInform() bool {
+	return ra == "Inform" || ra == "inform"
+}
+
+type NamespaceSelector struct {
+	// FUTURE: enhance this per
+	// https://github.com/open-cluster-management-io/enhancements/tree/main/enhancements/sig-policy/62-namespace-labelselector
+
+	// Include is a list of namespaces the policy should apply to.
+	Include []NonEmptyString `json:"include,omitempty"`
+
+	// Exclude is a list of namespaces the policy should _not_ apply to.
+	Exclude []NonEmptyString `json:"exclude,omitempty"`
+}
+
+//+kubebuilder:validation:MinLength=1
+type NonEmptyString string
+
+// PolicyCoreStatus defines fields that policies should implement as part of
+// the Open Cluster Management policy framework.
 type PolicyCoreStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// ComplianceState indicates whether the policy is compliant or not.
+	// Accepted values include: Compliant, NonCompliant, and UnknownCompliancy
+	ComplianceState ComplianceState `json:"compliant,omitempty"`
 }
+
+//+kubebuilder:validation:Enum=Compliant;NonCompliant;UnknownCompliancy
+type ComplianceState string
+
+const (
+	// Compliant indicates that the policy controller determined there were no
+	// violations to the policy in the cluster.
+	Compliant ComplianceState = "Compliant"
+
+	// NonCompliant indicates that the policy controller found an issue in the
+	// cluster that is considered a violation.
+	NonCompliant ComplianceState = "NonCompliant"
+
+	// UnknownCompliancy indicates that the policy controller could not determine
+	// if the cluster has any violations or not.
+	UnknownCompliancy ComplianceState = "UnknownCompliancy"
+)
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
